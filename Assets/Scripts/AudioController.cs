3dc05f707a2bc335feab129f0603aff5187775bf
jleeindustry <jlee.industry@gmail.com>
2019-03-31 12:33:19 -0600

    Initial commit

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    //Music featured is by Pictures of the Floating World
+    //Surge and Swell mp3 taken from:
+    //https://freemusicarchive.org/music/Pictures_of_the_Floating_World/Surge_and_Swell/
+    //Licensed under: Attribution-Noncommercial-Share Alike 3.0 United States License.
+
+    // Documentation: 
+    // GetSpectrumData: https://docs.unity3d.com/ScriptReference/AudioListener.GetSpectrumData.html
+    // FFTWindow: https://docs.unity3d.com/ScriptReference/FFTWindow.html
+
+    // variables
+    public AudioSource source;
+    //all samples, must be power of 2
+    public float[] samples = new float[512];
+    //arbitrary subset of samples (chosen because there are 5 cubes in example
+    public int[] tenSamples = new int[5];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get audio source
+        source.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get spectrum
+        // sampels = float array power of 2, min = 64, max 8192
+        // channel = channel from the sample
+        // FFTWindow = reduce leakage of signals across frequency bands
+
+        //try different windows to see different results.
+        //read about concept of each windiw here
+        // https://en.wikipedia.org/wiki/Window_function
+        //see docs linked on line 9 for more about windows in unity.
+        source.GetSpectrumData(samples, 0, FFTWindow.Hanning);
+
+        // Get the first ten numbers of sample and cast to int
+        for(int i = 0; i < tenSamples.Length; i++)
+        {
+            tenSamples[i] = (int)(samples[i]*100);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // variables
+    public GameObject audSour;
+    private int[] audioData;
+    public GameObject cube1;
+    public GameObject cube2;
+    public GameObject cube3;
+    public GameObject cube4;
+    public GameObject cube5;
+
+    private float changeAmount = 20.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get array from audioController
+        audioData = audSour.GetComponent<AudioController>().tenSamples;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // compare first 5 results from array
+        int audioArrayIdx = 0;
+        // you know a better way than linking all these up to find al the cubes in an array for you
+        //however this is not ultimately how you will be using this anyway, so it does not matter much
+        GameObject[] cubes = new GameObject[] { cube1, cube2, cube3, cube4, cube5 };
+        foreach (GameObject cube in cubes)
+        {
+
+            Debug.Log(audioData[audioArrayIdx]);
+            float cubeChange = 0.0f;
+
+            // check if value = even, move cube right
+            // else if value = odd, move cube left
+            // else default change 0 does not move cube
+
+            if ((audioData[audioArrayIdx] % 2) == 0 && audioData[audioArrayIdx] != 0)
+            {
+                //move cube more or less by multiplying it by current audionumber
+                cubeChange = changeAmount * audioData[audioArrayIdx];
+            }
+            else if ((audioData[audioArrayIdx] % 2) == 1)
+            {
+                //move cube more or less by multiplying it by current audionumber
+                cubeChange = -1 * changeAmount * audioData[audioArrayIdx];
+            }
+
+            Vector2 calcPos = cube.transform.right * Time.deltaTime * cubeChange;
+            calcPos.y = cube.transform.position.y;
+            cube.transform.position = calcPos;
+
+            audioArrayIdx++;
+        }
+    }
+}
